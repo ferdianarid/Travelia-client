0349d64b4ec81a394d2cf787273d8480ce2becb0
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-19 23:15:40 +0700

    create destination card components

---
+++ src/components/Destination.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Destination = () => {
+       return (
+              <React.Fragment>
+                     <div className="max-w-xs bg-blue-50 rounded-md border border-gray-200">
+                            <img src="https://cdn.idntimes.com/content-images/community/2019/02/2018-02-01-39875-1517462499-large-60d97e6631e637497c2db252b926af17_600x400.jpg" alt="bali" />
+                            <div className="heading p-4">
+                                   <h1 className='text-xl font-bold text-gray-800'>Bali National Beach</h1>
+                                   <p className="py-2 font-bold text-gray-700 text-sm">Bali, Indonesia</p>
+                                   <p className='text-md text-gray-600 leading-relaxed'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias reiciendis vero dolor eum provident sint nostrum quo illo aliquid!</p>
+                            </div>
+                     </div>
+              </React.Fragment>
+       )
+}
+
+export default Destination
