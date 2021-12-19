029b46cc1f2b017717d0795307c591980c5eec3e
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-19 22:51:58 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1 className="text-4xl font-bold text-blue-800">Hello World</h1>
+        )
+    }
+}
+
+export default App;
