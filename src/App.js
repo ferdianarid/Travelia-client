a4a7f0943c48f78bd758630a99091ca15011eb39
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-20 09:41:07 +0700

    update homepage

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends React.Component {
         return (
 		 <React.Fragment>
 			 <div className="m-8 flex md:flex-row flex-col items-center gap-x-5 gap-y-5">
-		 		<Destination heading="Bali National Beach" subheading="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias reiciendis vero dolor eum provident sint nostrum quo illo aliquid!" location="Bali, Indonesia" price="88500"  images="https://cdn.idntimes.com/content-images/community/2019/02/2018-02-01-39875-1517462499-large-60d97e6631e637497c2db252b926af17_600x400.jpg" />
-				 <Destination heading="Raja Ampat Lake" subheading="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias reiciendis vero dolor eum provident sint nostrum quo illo aliquid!" location="Raja Ampat, Indonesia" price="92500"  images="https://img.beritasatu.com/cache/beritasatu/910x580-2/301481430953.jpg" />
+		 		<Destination heading="Bali National Beach" subheading="Lorem ipsum dolor sit ameddrea consectetur adipisicing elit." location="Bali, Indonesia" price="88500"  images="https://cdn.idntimes.com/content-images/community/2019/02/2018-02-01-39875-1517462499-large-60d97e6631e637497c2db252b926af17_600x400.jpg" />
+				 <Destination heading="Raja Ampat Lake" subheading="Lorem ipsum dolor sit ameddrea consectetur adipisicing elit." location="Raja Ampat, Indonesia" price="92500"  images="https://img.beritasatu.com/cache/beritasatu/910x580-2/301481430953.jpg" />
 			 </div>
 		 </React.Fragment>
         )
