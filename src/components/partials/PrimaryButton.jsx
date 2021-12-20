b9c79e156860fd9fa0408978195c3b1409ba1a68
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-20 09:39:27 +0700

    update primary button

--- src/components/partials/PrimaryButton.jsx
+++ src/components/partials/PrimaryButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
+// Primary Button Component
 const PrimaryButton = ( props ) => {
        return (
               <React.Fragment>
-                     <button onClick={props.onClick} className='py-2 px-4 rounded-md bg-blue-600 border-none text-white font-medium hover:bg-blue-800 transition-all'>{ props.buttonText }</button>
+                     <button onClick={props.onClick} className='py-2 w-full px-4 rounded-md bg-blue-600 border-none text-white font-medium hover:bg-blue-800 transition-all'>{ props.buttonText }</button>
               </React.Fragment>
        )
 }
