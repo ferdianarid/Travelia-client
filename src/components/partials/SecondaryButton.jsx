be69ddd493bb0732ecc188bca1c95a61f4379d0c
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-22 17:43:49 +0700

    update secondary button

--- src/components/partials/SecondaryButton.jsx
+++ src/components/partials/SecondaryButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SecondaryButton = ({ children }) => {
        return (
-              <button className="bg-white hover:bg-blue-50 py-2 px-4 md:px-6 rounded-md border border-blue-100 mt-5 font-bold text-blue-600 ml-2 md:ml-4">{ children }</button>
+              <button className="bg-white hover:bg-blue-50 py-2 px-4 md:px-6 rounded-md mt-5 font-bold text-blue-600 ml-2 md:ml-4">{ children }</button>
        )
 }
 
