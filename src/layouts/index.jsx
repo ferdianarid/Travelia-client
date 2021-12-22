8d05c0d88613e635daa3e5ccc90b526f268d2e23
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-22 11:37:15 +0700

    update layouts

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Navbar from "../components/Navbar"
 
 const LayoutsIndex = ({ children }) => {
        return (
               <React.Fragment>
-                     <Navbar />
                      { children }
               </React.Fragment>
        )
