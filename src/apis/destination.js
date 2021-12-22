ed621648dde1a7812bd108f2508fe69bd0658e1e
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-22 23:50:44 +0700

    add apis

---
+++ src/apis/destination.js
@@ -0,0 +1,28 @@
+const items = [{
+        images: "https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+        title: "Florida Beach",
+        subtitle: "Florida beach is beach who located in los angeles usa. There are many visitor in weekend",
+        price: "$200",
+        location: "Los Angeles",
+        grade: "Expensive",
+        remaining: "5 Remaining"
+    },
+    {
+        images: "https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+        title: "Florida Beach",
+        subtitle: "Florida beach is beach who located in los angeles usa. There are many visitor in weekend",
+        price: "$200",
+        location: "Los Angeles",
+        grade: "Expensive",
+        remaining: "5 Remaining"
+    },
+    {
+        images: "https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+        title: "Florida Beach",
+        subtitle: "Florida beach is beach who located in los angeles usa. There are many visitor in weekend",
+        price: "$200",
+        location: "Los Angeles",
+        grade: "Expensive",
+        remaining: "5 Remaining"
+    }
+]
