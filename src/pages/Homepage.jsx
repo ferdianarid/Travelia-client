7c1eb8aabd550cc27a2d9f971f605a90509c72a5
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-22 23:52:49 +0700

    update homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,6 +6,15 @@ import LayoutsIndex from "../layouts"
 // impoer Partnership
 import Partnership from '../components/Partnership'
 
+// import Card Tickets
+import CardTickets from "../components/CardTickets"
+
+// import details button
+import DetailsButton from "../components/partials/DetailsButton"
+
+// import facility components
+import FacilitySupport from "../components/FacilitySupport"
+
 // import banner images
 import BannerImages from "../images/banner.jpg"
 import DotsImage from "../images/dots.png"
@@ -23,10 +32,36 @@ import Subheading from '../components/partials/Subheading'
 // import button primary \ secondary
 import PrimaryButton from '../components/partials/PrimaryButton'
 import SecondaryButton from '../components/partials/SecondaryButton'
+
+// import Recent Tabs
 import RecentTabs from '../components/RecentTabs'
+
+// import images
+import verified from "../images/verified.svg"
+import insurance from "../images/insurance.svg"
 
 // homepage
 const Homepage = () => {
+       const items = [{
+              images: "https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+              title: "Florida Beach",
+              subtitle: "Florida beach is beach who located in los angeles usa",
+              category: "Beach",
+              price: "$200",
+              location: "Los Angeles",
+              grade: "Expensive",
+              remaining: "5 Remaining"
+          },
+          {
+              images: "https://images.unsplash.com/photo-1515238152791-8216bfdf89a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+              title: "Nevada Beach",
+              subtitle: "Nevada beach is beach who located in los angeles usa",
+              category: "Beach",
+              price: "$250",
+              location: "Nevada City",
+              grade: "Expensive",
+              remaining: "7 Remaining"
+          }]
        return (
               <React.Fragment>
                      <LayoutsIndex>
@@ -38,7 +73,7 @@ const Homepage = () => {
                                           <Subheading>We will provide a travel tools and help you to find a powerful and beautiful destination in indonesia.</Subheading>
                                           {/* button call to action */}
                                           <div className="mt-2">
-                                                 <PrimaryButton>Find now</PrimaryButton>
+                                                 <PrimaryButton>Get the ticket</PrimaryButton>
                                                  <SecondaryButton>Guide me</SecondaryButton>
                                           </div>
                                    </div>
@@ -54,24 +89,12 @@ const Homepage = () => {
                             {/*  Features*/}
                             <div className="w-full flex flex-col md:flex-row justify-between items-center">
                                    <div className="w-full md:w-1/3 mt-80 md:mt-0">
-                                          <Heading>Promo for New Year.</Heading>
-                                          <Subheading>We provide bundling promo for new traveller</Subheading>
+                                          <Heading>Facility will you get.</Heading>
+                                          <Subheading>We always prioritize and support visitor with any facilitation</Subheading>
                                    </div>
-                                   <div className="w-full md:w-1/3 mt-8 md:mt-0 bg-blue-50 rounded-md p-6 mx-4">
-                                          <svg xmlns="http://www.w3.org/2000/svg" class="text-blue-600 h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
-                                          <path fill-rule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                                          </svg>
-                                          <p className="font-bold text-xl py-4">Verified Destination</p>
-                                          <Subheading>Lorem ipsum dolor sit amet consec, adipisicing elit. Et, vitae laborum aspernatur dolor dolore eligendi</Subheading>
-                                   </div>
-                                   <div className="w-full md:w-1/3 mt-8 md:mt-0 bg-blue-50 rounded-md p-6">
-                                          <svg xmlns="http://www.w3.org/2000/svg" class="text-blue-600 h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
-                                          <path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd" />
-                                          <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
-                                          </svg>
-                                          <p className="font-bold text-xl py-4">Travel Issurance</p>
-                                          <Subheading>Lorem ipsum dolor sit amet consec, adipisicing elit. Et, vitae laborum aspernatur dolor dolore eligendi</Subheading>
-                                   </div>
+                                   {/* facility components */}
+                                   <FacilitySupport title="Verified Destination" images={verified} alt="verified" subtitle="Lorem ipsum dolor sit amet consec, adipisicing elit. Et, vitae laborum aspernatur dolor dolore eligendi" />
+                                   <FacilitySupport title="Travel Issurance" images={insurance} alt="insurance" subtitle="Lorem ipsum dolor sit amet consec, adipisicing elit. Et, vitae laborum aspernatur dolor dolore eligendi" />
                             </div>
                             {/*  Preview Section*/}
                             <div className="w-full flex flex-col md:flex-row my-24 relative">
@@ -106,7 +129,8 @@ const Homepage = () => {
                                    <Subheading>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et non tempora natus voluptas aspernatur fugiat quaerat! Incidunt dolorem culpa nostrum sint repudiandae</Subheading>
                                    </div>
                             </div>
-                            <div className="w-full flex flex-col md:flex-row mb-24 mt-24 md:mt-96">
+                            {/* Frequently Asked Question */}
+                            <div className="w-full flex flex-col md:flex-row mb-10 mt-24 md:mt-96">
                                    <div className="w-full md:w-1/2 mt-0 md:mt-10">
                                           <Heading>Frequently Asked Question.</Heading>
                                           <Subheading>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos ea ipsam itaque nam perspiciatis nobis repellendus fuga rem veniam dolorum?</Subheading>
@@ -114,6 +138,59 @@ const Homepage = () => {
                                    <div className="w-full md:w-1/2 md:pl-10">
                                           <RecentTabs />
                                    </div>
+                            </div>
+                            {/* Card Destination */}
+                            <div className="w-full">
+                                   <Heading>Grab your ticket now.</Heading>
+                                   <Subheading>Lets grab fast your vacation ticket and enjoy your holiday with family</Subheading>
+                            </div>
+                            <div className="w-full mt-10 md:mt-14 mb-14 flex flex-col md:flex-row justify-between items-center">
+                                   {/* Card */}
+                                   <div className="w-72 md:w-64 bg-white shadow-2xl p-4 rounded-xl h-auto relative">
+                                          <img className='w-full h-40 rounded-xl object-cover' src={travelOne} alt="travel" />
+                                          <div className="absolute z-10 left-6 top-6 py-1 px-3 bg-blue-600 text-white text-sm font-bold rounded-xl">Mountain</div>
+                                          {/* card body */}
+                                          <div className="py-4">
+                                                 <div className="flex items-center justify-between">
+                                                 <p className="text-md font-extrabold text-blue-600">Mount Everest</p>
+                                                 <p className="text-xl font-bold text-blue-600">$355</p>
+                                                 </div>
+                                                 <p className="text-xs flex items-center font-bold text-gray-800 pb-3 pt-1">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                 </svg>
+                                                 Location : Everest Nepal</p>
+                                                 <Subheading>Lorem ipsum dolor sit amet amet consectetur adipisicing elit nika.</Subheading>
+                                                 <div className="flex items-center justify-between mt-5 text-blue-600">
+                                                        <div className="flex flex-col">
+                                                        <div className="flex items-center mr-6">
+                                                               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                               </svg>
+                                                               <p className="text-xs">Expensive</p>
+                                                        </div>
+                                                        <div className="flex items-center mt-2">
+                                                               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                                                               </svg>
+                                                               <p className="text-xs">8 Remaining</p>
+                                                        </div>
+                                                        </div>
+                                                 {/* button */}
+                                                 <DetailsButton>View Details</DetailsButton>
+                                                 </div>
+                                          </div>
+                                   </div>
+                                   {/* Card */}
+                                   {
+                                          items.map(destinate => (
+                                                 <CardTickets images={destinate.images} title={destinate.title} category={destinate.category} price={destinate.price} description={destinate.subtitle} remaining={destinate.remaining} grade={destinate.grade} location={destinate.location}  />
+                                          ))
+                                   }
+                            </div>
+                            <div className="w-full mx-auto mt-4 text-center">
+                                   <DetailsButton>Show more</DetailsButton>
                             </div>
                      </LayoutsIndex>
               </React.Fragment>
