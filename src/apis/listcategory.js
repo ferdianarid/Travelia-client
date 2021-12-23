e1ade3dadbfe10d3d1c761f4da7e156d83213c94
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-24 01:05:23 +0700

    add apis list cat

---
+++ src/apis/listcategory.js
@@ -0,0 +1,25 @@
+const category = [{
+        id: 1,
+        url: `https://images.unsplash.com/photo-1511497584788-876760111969?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80`,
+        title: "Nevada Forest",
+        category: "Forest",
+        subtitle: "Lorem ipsum dolor site amet.",
+        location: "Nevada USA"
+    },
+    {
+        id: 2,
+        url: `https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80`,
+        title: "Miami Beach",
+        category: "Beach",
+        subtitle: "Lorem ipsum dolor site amet.",
+        location: "Miami USA"
+    },
+    {
+        id: 3,
+        url: `https://images.unsplash.com/photo-1511497584788-876760111969?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80`,
+        title: "Denver Forest",
+        category: "Forest",
+        subtitle: "Lorem ipsum dolor site amet.",
+        location: "Denver USA"
+    }
+]
